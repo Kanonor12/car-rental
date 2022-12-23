ee21d44572e485507369f90c230a4ec86e02e0c3
Kanono Ramashamole <kanono@gmail.com>
2022-12-23 12:40:27 +0200

    Implement error handling

---
+++ server/src/databases/mongoDBConnect.js
@@ -0,0 +1,16 @@
+import expres from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import pkg from 'mongoose';
+
+dotenv.config();
+const { connection } = pkg;
+
+export const mongoDBConnect = async (req, res, next) => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log(`Database connected at ${connection.host}:${connection.port}`)
+    } catch (error) {
+        throw new error
+    }
+}
--- server/src/index.js
+++ server/src/index.js
@@ -2,14 +2,44 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+// Internal imports
+import { mongoDBConnect } from './databases/mongoDBConnect.js';
 
 dotenv.config()
 
-const PORT = 5001
+const PORT = process.env.PORT || 5000
 
 const app = express();
 
+mongoose.set('strictQuery', true);
+
+// Middlewares
+
+app.use(cookieParser())
+//app.use(express.json());
+app.use(bodyParser.json({ limit: "30mb", extended: false }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: false }))
+app.use(express.urlencoded({extended: false}));
+app.use(cors())
+
+//app.use('/posts', router)
+
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong";
+    
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    })
+})
+
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
+    mongoDBConnect()
    
 })
